6a2f6a27a29caf04c16833a599f7c2d8184d4e2c
Husenica, Jan <husenica.jan@gmail.com>
2018-07-26 16:43:50 +0200

    css updates and set pagination

--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,16 @@ window.addEventListener("DOMContentLoaded", function() {
   webResultSet = new ResultSet(document.getElementById(Constants.WEB_RESULT_DIV_ID), WebResult, Constants.WEB_PER_PAGE)
 }, false);
 
+document.getElementById(Constants.SEARCH_INPUT_ID).onkeydown = function(event) {
+    if (event && event.keyCode === 13) {
+        onSearch();
+    }
+};
+
 function onSearch() {
   const inputValue = document.getElementById(Constants.SEARCH_INPUT_ID).value;
   if(inputValue) {
-    // restApi.executeQuery(inputValue, Constants.QUERY_TYPE_IMG, preProcessImgResult);
+    restApi.executeQuery(inputValue, Constants.QUERY_TYPE_IMG, preProcessImgResult);
     restApi.executeQuery(inputValue, Constants.QUERY_TYPE_WEB, preProcessWebResult);
   }
 }
@@ -30,13 +36,34 @@ function preProcessWebResult(items) {
     return item;
   });
   webResultSet.setResults(webArr);
+  removePagination();
   addWebResultPagination(Math.ceil(items.length/ Constants.WEB_PER_PAGE))
 }
 
-function addWebResultPagination(pageCount) {
-    console.log(pageCount)
+function removePagination() {
+    const indexes = document.getElementById(Constants.PAGINATION_DIV_ID).getElementsByClassName('page-index');
+    while (indexes.length > 0) {
+        indexes.shift().remove();
+    }
 }
 
-function setPagination() {
-    webResultSet.setPage(1);
+function addWebResultPagination(pageCount) {
+    const paginationDiv = document.getElementById(Constants.PAGINATION_DIV_ID);
+
+    for(var i = 0; i< pageCount; i++) {
+        var pageIndex = document.createElement('a');
+        pageIndex.innerText = i + 1;
+        pageIndex.className = 'page-index';
+        pageIndex.setAttribute('pageIndex', i);
+        pageIndex.onclick = function () {
+            var index = this.getAttribute('pageIndex');
+            setPagination(index);
+        };
+
+        paginationDiv.appendChild(pageIndex);
+    }
+}
+
+function setPagination(index) {
+    webResultSet.setPage(index);
 }
